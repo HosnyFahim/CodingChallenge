ce481428950be3351d7f5c6a8ad8ad5067e6ec10
HosnyFahim <hosny.fahim1990@gmail.com>
2022-08-22 22:18:41 +0200

    finale update

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-bundesländer = [];
+let bundesländer = [];
+let letters = [];
 
 async function init() {
     let response = await fetch('./bundesländer.json');
@@ -7,15 +8,37 @@ async function init() {
 
 }
 
-function render() {
+function render(filter) {
     let content = document.getElementById('content');
     content.innerHTML = '';
 
     for (let i = 0; i < bundesländer.length; i++) {
         const land = bundesländer[i];
         const population = (land['population'] + '').replace(' , ', ' , ');
-        content.innerHTML += generateLink(land, population);
+        const firstLetter = land['name'].charAt(0); // first letter as String
 
+        if (!filter || filter == firstLetter) {
+            content.innerHTML += generateLink(land, population);
+        }
+
+        if (!letters.includes(firstLetter)) {
+            letters.push(firstLetter);
+        }
+    }
+    renderLetter();
+}
+
+function setFilter(letter) {
+    render(letter);
+}
+
+function renderLetter() {
+    let letterbox = document.getElementById('letterBox');
+    letterbox.innerHTML = '';
+
+    for (let i = 0; i < letters.length; i++) {
+        const letter = letters[i];
+        letterbox.innerHTML += `<div onclick="setFilter('${letter}')" class="letter">${letter}</div>`;
     }
 }
 
